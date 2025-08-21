fa18fd00aa30f7a7657e6a82a773ac6985d9e853
ChaimShvadron <s0527649958@gmail.com>
2025-08-21 12:31:00 +0300

    Add WeaponProcessor class to identify weapons in text

--- hostile-tweets-ex/app/processors/weapon_processor.py
+++ hostile-tweets-ex/app/processors/weapon_processor.py
@@ -1,1 +1,15 @@
+class WeaponProcessor:
+    def __init__(self, weapon_list_path):
+        weapon_list = []
+        with open(weapon_list_path, encoding='utf-8') as f:
+            for line in f:
+                weapon = line.strip()
+                if weapon:
+                    weapon_list.append(weapon)
+        self.weapon_list = weapon_list
 
+    def find_weapon(self, text):
+        for weapon in self.weapon_list:
+            if weapon in text:
+                return weapon
+        return None
