b7b3f25fe82237fd01b1226885286eed277ba001
ChaimShvadron <s0527649958@gmail.com>
2025-08-21 12:12:20 +0300

    Add RareWordProcessor class to identify rare words in text

--- hostile-tweets-ex/app/processors/rare_word_processor.py
+++ hostile-tweets-ex/app/processors/rare_word_processor.py
@@ -1,1 +1,8 @@
-
+class RareWordProcessor:
+    def find_rare_word(self, text):
+        words = text.split()
+        freq = {}
+        for word in words:
+            freq[word] = freq.get(word, 0) + 1
+        rare_word = min(freq, key=freq.get) if freq else None
+        return rare_word
