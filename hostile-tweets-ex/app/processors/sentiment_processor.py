7b931569589ff0aa80222970634711351f8aaa28
ChaimShvadron <s0527649958@gmail.com>
2025-08-21 12:25:53 +0300

    Implement SentimentProcessor class for sentiment analysis

--- hostile-tweets-ex/app/processors/sentiment_processor.py
+++ hostile-tweets-ex/app/processors/sentiment_processor.py
@@ -1,1 +1,19 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+class SentimentProcessor:
+
+	def __init__(self):
+		nltk.download('vader_lexicon', quiet=True)
+		self.analyzer = SentimentIntensityAnalyzer()
+
+	def get_sentiment(self, text):
+		score = self.analyzer.polarity_scores(text)
+		compound = score['compound']
+		if compound >= 0.5:
+			return 'Positive'
+		elif compound <= -0.5:
+			return 'Negative'
+		else:
+			return 'Neutral'
 
